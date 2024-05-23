97fde8b73c1fe90335eee87a3685b04322a5708e
KrishnaGupta0405 <161935799+KrishnaGupta0405@users.noreply.github.com>
2024-05-23 18:36:51 +0530

    Video 7 completed props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <h3> this is with using prop types</h3> */}
+      <Navbar title="TextUtils" aboutText="About us"/>
+
+      <h1>hi handsome how are you?</h1>
+    </>
   );
 }
 
